285f4e3024546b2dea92be9b2067ad15af95604a
Matt Bengston <mattb@unity3d.com>
2017-05-09 08:07:55 +0300

    Added readme copying and timestamps to deployment script, updated about page

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Function for easier timestamp inclusion
 timestamp() {
 	date +"%d.%m.%Y_%H:%M"
 }
 
+# Run NPM install then build the site to ./.deploy_git
+# @TODO: Automate creation of this post-install
 npm install && npm run deploy
 
+# Move the readme into the deployment directory for consistency
+cp README.md ./.deploy_git/README.md
+
+# Jump into deployment directory, add everything then push it up to Github
 cd ./.deploy_git
 git add --all
 git commit -S -m "Blog update @ $(timestamp)"
--- source/about.md
+++ source/about.md
@@ -7,3 +7,5 @@ category: About
 Hey there! My name is **Matt Bengston**, and I'm a **software developer**, **hobbyist indie game developer**, and **pixel artist** based out of Finland.
 
 I currently work at [Unity Technologies Finland](https://unity3d.com/helsinki/) in Helsinki, and in my spare time I work on pixel art, indie game projects, miscellaneous apps and project tooling.
+
+My portfolio may be a little bit out of date, but you can visit it at [mattbengston.com](http://mattbengston.com).
